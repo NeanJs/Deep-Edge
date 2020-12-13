667a9fc91e4bc64760cd04d2844c7fd7acbf7768
NeanJs <neancodes@gmail.com>
2020-12-13 22:54:56 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Deep-Edge/precache-manifest.4d60e5115dfdca7f1e16c8b59d42e6a9.js"
+  "/Deep-Edge/precache-manifest.ea9d79c099a885013fb3c0d99bc2aca0.js"
 );
 
 self.addEventListener('message', (event) => {
